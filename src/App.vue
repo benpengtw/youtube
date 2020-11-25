d43bb75d2a2e50572efa5f26571cbe6fc0307853
HNGF\ben.peng <ben.peng@hngf.com.tw>
2020-11-25 17:51:49 +0800

    add paginate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <paginate
+      :page-count="20"
+      :page-range="3"
+      :margin-pages="2"
+      :click-handler="clickCallback"
+      :prev-text="'Prev'"
+      :next-text="'Next'"
+      :container-class="'pagination'"
+      :page-class="'page-item'"
+    >
+    </paginate>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Paginate from "./components/Paginate.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: {
+      Paginate,
+    },
+    methods: {
+      clickCallback(pageNum) {
+        console.log(pageNum);
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,64 @@
+const compressionPlugin = require("compression-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  productionSourceMap: false,
+
+  lintOnSave: true,
+
+  // css相关配置
+  css: {
+    extract: false,
+    loaderOptions: {
+      postcss: {
+        plugins: [require("autoprefixer")],
+      },
+    },
+  },
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.resolve(__dirname, "src/assets/style/common.less")],
+    },
+  },
+  configureWebpack: (config) => {
+    // 生产环境去除console.log debugger
+    if (process.env.NODE_ENV === "production") {
+      config.optimization.minimizer[0].options.terserOptions.compress.warnings = false;
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true;
+      config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = [
+        "console.log",
+      ];
+    }
+  },
+  chainWebpack: (config) => {
+    // 移除 prefetch 插件以減少帶寬消耗
+    config.plugins.delete("prefetch");
+    if (process.env.NODE_ENV == "production") {
+      config.plugin("compressionPlugin").use(
+        new compressionPlugin({
+          test: /\.js$|\.html$|\.css$|\.ttf$|\.woff$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false, // 不删除源文件
+        }),
+      );
+    }
+    config.module
+      .rule("images")
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) =>
+        Object.assign(options, {
+          limit: 7000, //bytes
+        }),
+      )
+      .end();
+    config.optimization.minimize(true);
+    config.optimization.splitChunks({
+      chunks: "all",
+    });
+  },
+};
