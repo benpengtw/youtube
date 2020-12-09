0f742af82cbe1a72a5c3dea45de64aef7940aeaa
benpengtw <pojtyk@gmail.com>
2020-12-09 14:11:35 +0800

    使用 m3u8 為來源

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -20,8 +20,8 @@ export default {
 				controls: true,
 				sources: [
 					{
-						src:"../assets/video.mp4",
-						type: "video/mp4"
+						src:"https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+						type: "application/x-mpegURL"
 					}
 				]
 			}
